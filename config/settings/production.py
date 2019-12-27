65c3796a48ae7a72b44e8f4e8bf2b99e5c8116e9
Aktar Zaman <aktarzaman@berkeley.edu>
2019-12-26 22:49:00 -0800

    trying to change the subject line

--- config/settings/production.py
+++ config/settings/production.py
@@ -117,7 +117,7 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Bangladesh @ Berkeley <noreply@aktarzaman.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="BSA @ UC Berkeley <noreply@aktarzaman.com>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
